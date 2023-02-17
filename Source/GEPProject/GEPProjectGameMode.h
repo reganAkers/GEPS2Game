f879324dfcb156828460b3c901d3c42b58d704b5
Regan Akers <71647055+reganAkers@users.noreply.github.com>
2023-02-17 14:46:55 +0000

    Added project

---
+++ Source/GEPProject/GEPProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GEPProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGEPProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGEPProjectGameMode();
+};
+
+
+
